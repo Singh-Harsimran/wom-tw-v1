8f6b5b8208e6724572d35913a9b9cd8500360c7d
simran <simran381@gmail.com>
2023-03-06 18:41:13 -0800

    Added laravel-mix minification

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+require('laravel-mix-compress');
 
 mix.js('src/js/app.js', 'assets')
    .sass('src/scss/app.scss', 'assets')
@@ -7,3 +8,6 @@ mix.js('src/js/app.js', 'assets')
       processCssUrls: false,
       postCss: [tailwindcss('tailwind.config.js')]
    });
+   mix.minify('assets/app.css')
+
+   
